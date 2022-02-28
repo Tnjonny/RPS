bda2acc9d900861a0207f74e207844bf19bdbe5e
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-28 19:30:43 +0000

    commit

---
+++ rps.js
@@ -0,0 +1,5 @@
+const computer = document.getElementById('computer-choice');
+const user = document.getElementById('user-choice');
+const results = document.getElementById('results');
+
+const possibleChoices = document.querySelectorAll('button');
