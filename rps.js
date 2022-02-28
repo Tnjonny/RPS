e2fb63edc3ac9cddb2d63e5d34f26ad33cf08cb1
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-02-28 19:46:00 +0000

    commit

--- rps.js
+++ rps.js
@@ -1,5 +1,13 @@
-const computer = document.getElementById('computer-choice');
-const user = document.getElementById('user-choice');
-const results = document.getElementById('results');
+const computerDisplay = document.getElementById('computer-choice');
+const userDisplay = document.getElementById('your-choice');
+const resultsDisplay = document.getElementById('results');
 
 const possibleChoices = document.querySelectorAll('button');
+let user;
+
+possibleChoices.forEach((possibleChoices) =>
+  possibleChoices.addEventListener('click', (e) => {
+    user = e.target.id;
+    userDisplay.innerHTML = user;
+  })
+);
