7c42fd5aa5f7d1e04211bae33cfe1935f7a31ee3
Tnjonny <79794963+Tnjonny@users.noreply.github.com>
2022-03-01 15:06:27 +0000

    commit

--- rps.js
+++ rps.js
@@ -1,6 +1,8 @@
 const computerDisplay = document.getElementById('computer-choice');
 const userDisplay = document.getElementById('your-choice');
 const resultsDisplay = document.getElementById('results');
+let userChoice;
+let computerChoice;
 
 const possibleChoices = document.querySelectorAll('button');
 let user;
@@ -9,5 +11,25 @@ possibleChoices.forEach((possibleChoices) =>
   possibleChoices.addEventListener('click', (e) => {
     user = e.target.id;
     userDisplay.innerHTML = user;
+    generateComputerChoice();
   })
 );
+
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
+  console.log(randomNumber);
+
+  if (randomNumber === 1) {
+    computerChoice = 'Rock';
+  }
+
+  if (randomNumber === 2) {
+    computerChoice = 'Scissors';
+  }
+
+  if (randomNumber === 3) {
+    computerChoice = 'Paper';
+  }
+
+  computerDisplay.innerHTML = computerChoice;
+}
